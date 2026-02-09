d44adf3702a7ed2dae6149ef0dd7933818df74c6
Noel Felix <nn.felix.25@gmail.com>
2026-02-08 21:03:59 -0800

    chore: remove logging of result in runAgent function

--- src/agent/run.ts
+++ src/agent/run.ts
@@ -43,7 +43,7 @@ export async function runAgent(
       //   },
       // },
     });
-    console.log(result);
+
     const toolCalls: ToolCallInfo[] = [];
     let currentText = '';
     let streamError: Error | null = null;
